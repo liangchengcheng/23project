004f2026f3d87b4080e6fd68aeef3d3d8f7e7175
liangchengcheng <1038127753@qq.com>
2017-10-29 19:57:24 +0800

    add java class file
    add java class file

---
+++ src/com/lcc/yuanxing/Client.java
@@ -0,0 +1,29 @@
+package com.lcc.yuanxing;
+
+import java.util.ArrayList;
+
+/**
+ * 主要用于细微变化的
+ */
+public class Client {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        // 家庭成员名单
+        ArrayList<String> famMem = new ArrayList<>();
+        famMem.add("Papa");
+        famMem.add("Mama");
+
+        // 创建初始简历
+        Resume resume1 = new Resume("Jobs");
+        resume1.setPersonal("Male", 26,famMem);
+        resume1.setWork("2013/8/1 - 2015/6/30", "Huawei");
+
+        // 通过简历1复制出简历2，并对家庭成员和工作经验进行修改
+        Resume resume2 = resume1.clone();
+        resume2.setName("Tom");
+        resume2.famMem.add("Brother");
+        resume2.setWork("2015/7/1 - 2016/6/30", "Baidu");
+
+        resume1.display();
+        resume2.display();
+    }
+}
---
+++ src/com/lcc/yuanxing/WorkExperience.java
@@ -0,0 +1,13 @@
+package com.lcc.yuanxing;
+
+/**
+ * 基本的简历表（模板）
+ */
+public class WorkExperience {
+
+    //工作的时间
+    public String timeArea = null;
+
+    //工作的公司
+    public String company = null;
+}
