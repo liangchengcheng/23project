e990596655e75065c191f4f530742b22753694b2
liangchengcheng <1038127753@qq.com>
2017-10-30 06:21:21 +0800

    add  java class file
    add  java class file

--- src/com/lcc/guanchazhe/Client.java
+++ src/com/lcc/guanchazhe/Client.java
@@ -1,11 +1,14 @@
 package com.lcc.guanchazhe;
 
+/**
+ * http://www.jasongj.com/design_pattern/observer/
+ */
 public class Client {
 
     public static void main(String[] args) {
         ITalent juniorEngineer = new JuniorEngineer();
         ITalent seniorEngineer = new SeniorEngineer();
-        
+
         ITalent architect = new Architect();
 
         AbstractHR subject = new HeadHunter();
